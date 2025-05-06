b21f099c28c542fed578181e855ea5d5fea92c87
bartek <dsw56002@student.dsw.edu.pl>
2025-05-06 18:51:47 +0200

    Dodano krótkie flagi jako aliasy

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date
 fi
 
-elif [[ "$1" == "--logs" ]]; then
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     num_files=100
     if [[ "$2" =~ ^[0-9]+$ ]]; then
         num_files=$2
@@ -14,8 +14,8 @@ elif [[ "$1" == "--logs" ]]; then
         echo -e "$filename\nskrypt.sh\n$(date)" > "$filename"
     done
 
-elif [[ "$1" == "--help" ]]; then
-    echo "--date        Wyświetla dzisiejszą datę"
-    echo "--logs [N]    Tworzy N plików logx.txt"
-    echo "--help        Wyświetla pomoc"
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "--date, -d        Wyświetla dzisiejszą datę"
+    echo "--logs [N], -l    Tworzy N plików logx.txt, domyślnie 100"
+    echo "--help, -h        Wyświetla pomoc"
 fi
