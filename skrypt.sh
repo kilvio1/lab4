344b21cb6b3f036153e65aae7e4457d617cad1e5
bartek <dsw56002@student.dsw.edu.pl>
2025-05-06 18:56:47 +0200

    Dodano --error/-e: tworzenie plików błędów

--- skrypt.sh
+++ skrypt.sh
@@ -24,3 +24,15 @@ elif [[ "$1" == "--init" ]]; then
     git clone "$(git config --get remote.origin.url)" cloned_repo
     echo "export PATH=\$PATH:$(pwd)/cloned_repo" >> ~/.bashrc
     echo "Ścieżka dodana do PATH (na stałe po ponownym zalogowaniu)."
+
+elif [[ "$1" == "--error" || "$1" == "-e" ]]; then
+    num_errors=100
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        num_errors=$2
+    fi
+
+    mkdir -p errorx
+    for i in $(seq 1 "$num_errors"); do
+        filename="errorx/error${i}.txt"
+        echo -e "$filename\n$0\n$(date)" > "$filename"
+    done
