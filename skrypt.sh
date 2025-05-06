81644dd3afc11b32efb8c9749f192877ae7c64e7
kilvio1 <bartlomiej.gorczyca11@gmail.com>
2025-05-06 19:26:34 +0200

    update skrypt.sh
    Popełniłem wiele błędów składniowych tworząc ten plik, ponieważ pisałem kod w blokach i nie aktualizowałem struktury z czasem. Po zakończeniu projektu pozwoliłem sobie uaktualnić plik w repozytorium już na stronie githuba. Mam nadzieję ze nie zmieni to oceny tego laboratorium.

--- skrypt.sh
+++ skrypt.sh
@@ -1,38 +1,52 @@
 #!/bin/bash
 
+function create_logs() {
+    local count=$1
+    for i in $(seq 1 "$count"); do
+        filename="log${i}.txt"
+        echo -e "$filename\nskrypt.sh\n$(date)" > "$filename"
+    done
+}
+
+function create_errors() {
+    local count=$1
+    mkdir -p errorx
+    for i in $(seq 1 "$count"); do
+        filename="errorx/error${i}.txt"
+        echo -e "$filename\n$0\n$(date)" > "$filename"
+    done
+}
+
 if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date
-fi
 
 elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     num_files=100
     if [[ "$2" =~ ^[0-9]+$ ]]; then
         num_files=$2
     fi
-    for i in $(seq 1 $num_files); do
-        filename="log${i}.txt"
-        echo -e "$filename\nskrypt.sh\n$(date)" > "$filename"
-    done
-
-elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    echo "--date, -d        Wyświetla dzisiejszą datę"
-    echo "--logs [N], -l    Tworzy N plików logx.txt, domyślnie 100"
-    echo "--help, -h        Wyświetla pomoc"
-fi
-
-elif [[ "$1" == "--init" ]]; then
-    git clone "$(git config --get remote.origin.url)" cloned_repo
-    echo "export PATH=\$PATH:$(pwd)/cloned_repo" >> ~/.bashrc
-    echo "Ścieżka dodana do PATH (na stałe po ponownym zalogowaniu)."
+    create_logs "$num_files"
 
 elif [[ "$1" == "--error" || "$1" == "-e" ]]; then
     num_errors=100
     if [[ "$2" =~ ^[0-9]+$ ]]; then
         num_errors=$2
     fi
+    create_errors "$num_errors"
 
-    mkdir -p errorx
-    for i in $(seq 1 "$num_errors"); do
-        filename="errorx/error${i}.txt"
-        echo -e "$filename\n$0\n$(date)" > "$filename"
-    done
+elif [[ "$1" == "--init" ]]; then
+    git clone "$(git config --get remote.origin.url)" cloned_repo
+    echo "export PATH=\$PATH:$(pwd)/cloned_repo" >> ~/.bashrc
+    echo "Ścieżka dodana do PATH (na stałe po ponownym zalogowaniu)."
+
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    echo "--date, -d         Wyświetla dzisiejszą datę"
+    echo "--logs [N], -l     Tworzy N plików logx.txt, domyślnie 100"
+    echo "--error [N], -e    Tworzy N plików errorx/errorx.txt, domyślnie 100"
+    echo "--init             Klonuje repo i dodaje do PATH"
+    echo "--help, -h         Wyświetla pomoc"
+
+else
+    echo "Nieznana opcja. Użyj --help, aby uzyskać listę dostępnych opcji."
+fi
+
