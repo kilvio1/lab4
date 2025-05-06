970cc7768fd1b8f1232dfd880bf3a4b073f689e8
bartek <dsw56002@student.dsw.edu.pl>
2025-05-06 18:36:19 +0200

    Dodano opcję --logs z obsługą liczby plików

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,13 @@
 if [[ "$1" == "--date" ]]; then
     date
 fi
+
+elif [[ "$1" == "--logs" ]]; then
+    num_files=100
+    if [[ "$2" =~ ^[0-9]+$ ]]; then
+        num_files=$2
+    fi
+    for i in $(seq 1 $num_files); do
+        filename="log${i}.txt"
+        echo -e "$filename\nskrypt.sh\n$(date)" > "$filename"
+    done
